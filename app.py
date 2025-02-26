eedb24651e9333a7b64c6b079f080aed58425b20
alihassanml <fa-22-bscs-294@lgu.edu.pk>
2025-02-26 14:48:34 +0500

    changes

--- app.py
+++ app.py
@@ -1,25 +1,90 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-import spacy
-from pypdf import PdfReader
+# Import library
+import re #regx library
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pypdf import PdfReader # Read pdf
+import streamlit as st
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-url = './resume.pdf'
-reader = PdfReader(url)
-read = reader.pages[0]
-text = read.extract_text()
+import pickle
+# load skill
+with open("./list/skills.pkl", "rb") as f:
+    skills = pickle.load(f)
+# load education    
+with open("./list/education.pkl", "rb") as f:
+    education = pickle.load(f)
+# load experience
+with open("./list/experience.pkl", "rb") as f:
+    experience = pickle.load(f)
 
 
-nlp = spacy.load("en_core_web_sm")
-print("Model Loaded Successfully!")
+# Read pdf 
+def pdf_read(url):
+    reader = PdfReader(url)
+    read = reader.pages[0]
+    text = read.extract_text()
+    return text
 
-import spacy
-import re
+# Extract Skill
+def extract_skills(text):
+    extracted_skills = [skill for skill in skills if re.search(rf'\b{skill}\b', text, re.IGNORECASE)]
+    return extracted_skills
 
-nlp = spacy.load("en_core_web_sm")
+#  Education Extraction Function
+def extract_education(text):
+    degrees = education["degrees"]
+    fields_of_study = education["fields_of_study"]
+    institutions = education["institutions"]
+    
+    extracted_degrees = [deg for deg in degrees if re.search(rf'\b{deg}\b', text, re.IGNORECASE)]
+    extracted_fields = [field for field in fields_of_study if re.search(rf'\b{field}\b', text, re.IGNORECASE)]
+    extracted_institutions = [inst for inst in institutions if re.search(rf'\b{inst}\b', text, re.IGNORECASE)]
+    
+    return {
+        "Degrees": extracted_degrees,
+        "Fields of Study": extracted_fields,
+        "Institutions": extracted_institutions
+    }
+
+# Extract experience
+def extract_experience(text):
+    extracted_experience = [exp for exp in experience if re.search(rf'\b{exp}\b', text, re.IGNORECASE)]
+    return extracted_experience
 
 
-doc = nlp(text)
+# Apply on my resume
+def extract_Resume(resume):
+    skill = extract_skills(resume)
+    education = extract_education(resume)
+    experience = extract_experience(resume)
+    
+    result = {
+        'skill': skill,
+        'education': education,
+        'experience': experience
+    }
 
-print(doc)
+    # Convert the result to JSON format
+    return result
+
+def cleanResume(txt):
+    cleanText = re.sub('http\S+\s', ' ', txt)
+    cleanText = re.sub('RT|cc', ' ', cleanText)
+    cleanText = re.sub('#\S+\s', ' ', cleanText)
+    cleanText = re.sub('@\S+', '  ', cleanText)  
+    cleanText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
+    cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText) 
+    cleanText = re.sub('\s+', ' ', cleanText)
+    return cleanText
+
+
+def get_ResumeSimilarity(myResume,jobDescription):
+    vector = TfidfVectorizer()
+    tfidf_vector = vector.fit_transform([myResume,jobDescription])
+    cosine_sim = cosine_similarity(tfidf_vector[0:1], tfidf_vector[1:2])
---
+++ frontend/model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean,Integer,Column,String,ForeignKey,Float,DateTime, Enum, Text,LargeBinary
+from sqlalchemy.sql import func 
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Sginup(Base):
+    __tablename__ = 'Sginup'
+    id = Column(Integer,primary_key=True,index=True)
+    username = Column(String,unique=True,index=True)
+    email = Column(String,unique=True,index=True)
+    organization = Column(String,index=True)
+    password = Column(String,index=True)
+    is_active = Column(Boolean, default=True)
+    
+    todos = relationship('Project',back_populates='owner')
