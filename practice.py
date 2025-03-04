13c331f6e17241f6ac2596a564810fac9e8c38f2
alihassanml <fa-22-bscs-294@lgu.edu.pk>
2025-03-05 00:15:00 +0500

    frontend

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
 from flair.models import SequenceTagger
 from flair.data import Sentence
-from pypdf import PdfReader # Read pdf
+from pypdf import PdfReader  # Read pdf
+
+# Load NER tagger
 tagger = SequenceTagger.load("ner")
 
 
@@ -10,10 +12,17 @@ def pdf_read(url):
     text = read.extract_text()
     return text
 
-url = 'resume.pdf'
-sentence = str(pdf_read(url))
 
+url = "resume.pdf"
+text = pdf_read(url)
+
+# Convert text into a Sentence object
+sentence = Sentence(text)
+
+# Perform Named Entity Recognition
 tagger.predict(sentence)
-for entity in sentence.get_spans('ner'):
+
+# Extract and print person's name
+for entity in sentence.get_spans("ner"):
     if entity.tag == "PER":  # PER = Person
         print(entity.text)
