320eb23b3f77fd11918fe3c7b5a10a2f142a23bb
alihassanml <alihassancsml@gmail.com>
2025-05-12 11:12:02 +0500

    website

--- website/backend/app.py
+++ website/backend/app.py
@@ -107,10 +107,10 @@ async def upload(
         extracted_job = pdf_read(job_location)
 
         result = resume_result(extracted_resume,extracted_job)
-        user_name = predict_name(extracted_resume) 
+        # user_name = predict_name(extracted_resume) 
 
         db_result = model.ResumeResult(
-            name=user_name,
+            name="ali hassan",
             result_json=json.dumps(result),
             rank = result['resume_rank']
         )
